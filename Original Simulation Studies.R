634654819f8692e72245cd00ddfb150862bdec4f
Your Name <satoj@lawrence.edu>
2019-07-23 13:20:10 -0500

    Added the function that outputs the ratios of MSPE & Avg.Width & Coverage Rate between Linear Regression and each of the other three, QRF & OOB & OOB:sym. Ignored the part that was computing the optimal node sizes using small data sets with n=50. To run properly, now the work space data "30 pattern retry" that contains the optimal node sizes for each data type obtained with n=2000 and #loop=5.

--- Original Simulation Studies.R
+++ Original Simulation Studies.R
@@ -1,6 +1,3 @@
-# Need to load this work environment in order to get the right nodesizes
-load("30 pattens retry.RData")
-
 library(randomForestSRC)
 ##### CR: computes the coverage rate of the model under the given data #####
 CR <- function(quantile, testdata){
@@ -131,8 +128,8 @@ mTabs <- function(n, np, sig, equation, loops, ns, error){
     test  <- preserve[[2]]
     
     if(t==1){
-      storedata[(1:(n/2)),]      <- train
-      storedata[(((n/2)+1):n),] <- test
+    storedata[(1:(n/2)),]      <- train
+    storedata[(((n/2)+1):n),] <- test
     }
     
     for(k in 1:length(ns)){
@@ -163,7 +160,7 @@ error1 <- function(variable,n){
   
   #e <- c(rep(NA, dim(variable)[[1]])) 
   #for (k in 1:(dim(variable)[[1]])){ 
-  # e[k] <- rnorm(n=1, mean=0, sd=abs(variable[k,1]))
+   # e[k] <- rnorm(n=1, mean=0, sd=abs(variable[k,1]))
   #}
   return(e)
 }
@@ -173,9 +170,9 @@ error1 <- function(variable,n){
 error2 <- function(variable,n){
   e <- rnorm(n=dim(variable)[[1]], mean=0, sd=5)
   
-  #e <- c(rep(NA, dim(variable)[[1]])) 
+   #e <- c(rep(NA, dim(variable)[[1]])) 
   #for (k in 1:(dim(variable)[[1]])){ 
-  # e[k] <- rnorm(n=1, mean=0, sd=5)
+   # e[k] <- rnorm(n=1, mean=0, sd=5)
   #}
   return(e)
 }
@@ -188,7 +185,7 @@ error3 <- function(variable,n){
   
   #e <- c(rep(NA, dim(variable)[[1]])) 
   #for (k in 1:(dim(variable)[[1]])){ 
-  # e[k] <- rt(n=1, df=3)
+   # e[k] <- rt(n=1, df=3)
   #}
   return(e)
 }
@@ -199,7 +196,7 @@ error3 <- function(variable,n){
 error4 <- function(variable,n){
   #e <- c(rep(NA, dim(variable)[[1]])) 
   #for (k in 1:(dim(variable)[[1]])){ 
-  # e[k] <- rt(n=1, df=3) + rnorm(n=1, mean=0, sd=abs(variable[k,1]))
+   # e[k] <- rt(n=1, df=3) + rnorm(n=1, mean=0, sd=abs(variable[k,1]))
   #}
   e <- rt(n=dim(variable)[[1]], df=3) 
   + rnorm(n=dim(variable)[[1]], mean=0, sd=abs(variable[k,1]))
@@ -212,7 +209,7 @@ error4 <- function(variable,n){
 error5 <- function(variable,n){
   #e <- c(rep(NA, dim(variable)[[1]])) 
   #for (k in 1:(dim(variable)[[1]])){ 
-  # e[k] <- rexp(n=1,rate=2)-0.5
+   # e[k] <- rexp(n=1,rate=2)-0.5
   #}
   e <- rexp(n=dim(variable)[[1]],rate=2)-0.5
   
@@ -224,7 +221,7 @@ error5 <- function(variable,n){
 error6 <- function(variable,n){
   #e <- c(rep(NA, dim(variable)[[1]])) 
   #for (k in 1:(dim(variable)[[1]])){ 
-  # e[k] <- rexp(n=1,rate=2)-0.5 +runif(n=1, -abs(variable[k,1]), abs(variable[k,1]))
+   # e[k] <- rexp(n=1,rate=2)-0.5 +runif(n=1, -abs(variable[k,1]), abs(variable[k,1]))
   #}
   e <- rexp(n=dim(variable)[[1]],rate=2)-0.5 
   +runif(n=dim(variable)[[1]], -abs(variable[k,1]), abs(variable[k,1]))
@@ -245,7 +242,7 @@ eq1 <- function(variable, error){
   #y <- c(rep(NA, dim(variable)[[1]])) 
   y <-2*variable[,1] + 3 + error
   #for (k in 1:(dim(variable)[[1]])) {
-  # y[k] <- 2*variable[k,1] + 3 + error[k]
+   # y[k] <- 2*variable[k,1] + 3 + error[k]
   #}
   
   return(y)
@@ -254,7 +251,7 @@ eq1 <- function(variable, error){
 # eq2
 eq2 <- function(variable, error){
   y <- 0.1*(variable[,1]-7)^2-3*cos(variable[,1]) + 5*log(abs(variable[,1])) + 3 + error
-  
+
   #y <- c(rep(NA, dim(variable)[[1]])) 
   
   #for (k in 1:dim(variable)[[1]]) {
@@ -269,7 +266,7 @@ eq3 <- function(variable, error){
   y <- 2*variable[,1]+3*variable[,4]+4*variable[,6]-3*variable[,7]+ variable[,9] + error
   
   #for (k in 1:(dim(variable)[[1]])) {
-  # y[k] <- 2*variable[k,1]+3*variable[k,4]+4*variable[k,6]-3*variable[k,7]+ variable[k,9]   + error[k]
+   # y[k] <- 2*variable[k,1]+3*variable[k,4]+4*variable[k,6]-3*variable[k,7]+ variable[k,9]   + error[k]
   #}
   return(y)
 }
@@ -279,9 +276,9 @@ eq4 <- function(variable, error){
   y <- (variable[,1]-6)^2 + 12*cos(variable[,3])  + (variable[,7]-5)*(variable[,8]-3)  
   + 0.02*(variable[,10]-5)^5 + error
   #c(rep(NA,dim(variable)[[1]]))
-  
+   
   #for (k in 1:(dim(variable)[[1]])) {
-  # y[k] <- (variable[k,1]-6)^2 + 12*cos(variable[k,3]) 
+   # y[k] <- (variable[k,1]-6)^2 + 12*cos(variable[k,3]) 
   #+ (variable[k,7]-5)*(variable[k,8]-3) + 0.02*(variable[k,10]-5)^5 + error[k]
   #}
   return(y)
@@ -293,7 +290,7 @@ eq5 <- function(variable, error){
   y <- 10*exp(-variable[,1])*sin(variable[,1]) + error
   
   #for (k in 1:(dim(variable)[[1]])){ 
-  # y[k] <- 10*exp(-variable[k,1])*sin(variable[k,1]) + error[k]
+   # y[k] <- 10*exp(-variable[k,1])*sin(variable[k,1]) + error[k]
   #}
   
   return(y)
@@ -319,16 +316,16 @@ makedata <- function(n, np, sig, equation, error){
 
 ### Function to find optimal nodesizes and their mean ###
 #optimize <- function(loops, equation, n, np, error){
-# library(randomForestSRC)
-#ns <- c()
-
-#for (k in 1:loops) {
-# see <- makedata(n=n, np=np, sig=5, equation=equation, error=error)
-#ns[k]   <- tune(y~., see$train)$optimal[[1]]
-#ns[k+1] <- tune(y~., see$test )$optimal[[1]]
-#}
-
-#return(list("Avg.NS"=mean(ns), "NS"=ns))
+ # library(randomForestSRC)
+  #ns <- c()
+  
+  #for (k in 1:loops) {
+   # see <- makedata(n=n, np=np, sig=5, equation=equation, error=error)
+    #ns[k]   <- tune(y~., see$train)$optimal[[1]]
+    #ns[k+1] <- tune(y~., see$test )$optimal[[1]]
+  #}
+  
+  #return(list("Avg.NS"=mean(ns), "NS"=ns))
 #}
 
 ### optimal ns for each combination of base and error equation ###
@@ -337,9 +334,9 @@ errors <- list("error1"=list(),"error2"=list(),"error3"=list(),"error4"=list(),"
 
 #refer <- list("eq1"=errors,"eq2"=errors,"eq3"=errors,"eq4"=errors,"eq5"=errors)
 #for (k in 1:length(eqAll)) {
-# for(t in 1:length(errorAll)){
-#  refer[[k]][[t]] <- optimize(loops=2, equation=eqAll[[k]], n=50, np=nps[k], error=errorAll[[t]])
-#}
+ # for(t in 1:length(errorAll)){
+  #  refer[[k]][[t]] <- optimize(loops=2, equation=eqAll[[k]], n=50, np=nps[k], error=errorAll[[t]])
+  #}
 #}
 
 ### ns vectors ###
@@ -421,11 +418,9 @@ for (k in 1:length(eqAll)) {
   for(t in 1:length(errorAll)){
     d <- mTabs(n=2000, np=nps[k],  sig=5, equation=eqAll[[k]], loops=10, ns=ns[[k]][[t]], error=errorAll[[t]])
     stored[[k]][[t]] <- d$storeddata
-    dfs[[k]][[t]] <- tada(ns=ns[[k]][[t]], data=d)
+       dfs[[k]][[t]] <- tada(ns=ns[[k]][[t]], data=d)
   }
 }
-
-save(dfs, stored, file="OriginalSimulationStudies_Results.Rdata")
 
 ##### Plot #####
 
@@ -442,8 +437,8 @@ plotter <- function(data){
 Dplotter <- function(data){
   if(dim(data)[[2]]==2){
     return(list("X1/Y"=ggplot(data=data, aes(x=X1, y=Y)) + geom_point()
-    )
-    )}
+                )
+           )}
   else                {
     return(list("X1/Y" =ggplot(data=data, aes(x=X1,  y=Y)) + geom_point(),
                 "X2/Y" =ggplot(data=data, aes(x=X2,  y=Y)) + geom_point(),
@@ -456,8 +451,8 @@ Dplotter <- function(data){
                 "X8/Y" =ggplot(data=data, aes(x=X8,  y=Y)) + geom_point(),
                 "X9/Y" =ggplot(data=data, aes(x=X9,  y=Y)) + geom_point(),
                 "X10/Y"=ggplot(data=data, aes(x=X10, y=Y)) + geom_point()
-    )
-    )
+                )
+           )
   }
 }
 
@@ -465,8 +460,43 @@ Dplotter <- function(data){
 plots <- list("eq1"=errors,"eq2"=errors,"eq3"=errors,"eq4"=errors,"eq5"=errors)
 for (k in 1:length(eqAll)) {
   for(t in 1:length(errorAll)){
-    plots[[k]][[t]] <- list( "Three Values"=plotter(data=dfs[[k]][[t]]), "data plot"=Dplotter(stored[[k]][[t]]))
+   plots[[k]][[t]] <- list( "Three Values"=plotter(data=dfs[[k]][[t]]), "data plot"=Dplotter(stored[[k]][[t]]))
   }
 }
 
+
+##### Compare Avg. Wid #####
+
+### Function that returns ratios, their avg, min&max of the Avg.Width ### 
+widAnalysis <- function(data){
+  df <- data.frame(matrix(NA, nrow=dim(data)[[1]], ncol=dim(data)[[2]]))
+  colnames(df) <-  c("methods", "nodesize","MSPE","Avg.Wid", "CR")
+  df$methods  <- data$Method #methods
+  df$nodesize <- data$Nodesize #nodesizes
+  df[1:(dim(df)[[1]]/4), 3:4] <- data[1:(dim(df)[[1]]/4),2:3]
+  for (k in 1:3) {
+    df$MSPE   [((k*dim(data)[[1]]/4+1):((k+1)*dim(data)[[1]]/4))] <- data$Avg.MSPE[((k*dim(data)[[1]]/4+1)):((k+1)*dim(data)[[1]]/4)] / data$Avg.MSPE[1:(dim(data)[[1]]/4)]
+    df$Avg.Wid[((k*dim(data)[[1]]/4+1):((k+1)*dim(data)[[1]]/4))] <- data$Avg.Wid [((k*dim(data)[[1]]/4+1)):((k+1)*dim(data)[[1]]/4)] / data$Avg.Wid[1:(dim(data)[[1]]/4)]
+    #df$CR   [((k*dim(data)[[1]]/4+1):((k+1)*dim(data)[[1]]/4))] <- data$Avg.CR[((k*dim(data)[[1]]/4+1)):((k+1)*dim(data)[[1]]/4)] / data$Avg.CR[1:(dim(data)[[1]]/4)]
+  }
+  df$CR <- data$Avg.CR
+  dfwanted <- df[order(df$nodesize),]
+  
+  return(dfwanted)
+}
+
+# Summarize: all the ratios are in this list, for each of the 30 combinations. Only remains to extract the rows with the optimal nodesize, once the data simulation finishes
+width <- list("eq1"=lapply(X=dfs[[1]],widAnalysis),"eq2"=lapply(X=dfs[[2]],widAnalysis),"eq3"=lapply(X=dfs[[3]],widAnalysis),
+              "eq4"=lapply(X=dfs[[4]],widAnalysis),"eq5"=lapply(X=dfs[[5]],widAnalysis)  )
+
+# extract the ones with optimal nodesizes from each simulation #
+table <- data.frame(matrix(NA, nrow=4*4, ncol=5))
+colnames(table) <- colnames(width$Sim1)
+
+table[1:4,  ] <- width$Sim1[width$Sim1$nodesize==ns1[(length(ns1)+1)/2],]
+table[5:8,  ] <- width$Sim2[width$Sim2$nodesize==ns2[(length(ns2)+1)/2],]
+table[9:12, ] <- width$Sim3[width$Sim3$nodesize==ns3[2],]
+table[13:16,] <- width$Sim4[width$Sim4$nodesize==ns4[2],]
+
+# ALSO APPLY MAKE THE PLOTS FOR THE 30 COMBINATIONS ONCE THE SIMULATION IS COMPLETE
 
